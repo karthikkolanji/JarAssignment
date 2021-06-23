4610d838539530824fc1fff1d18df4c16a6b6365
karthik <karthik.kolanji90@gmail.com>
2021-06-23 23:43:31 +0530

    adds hilt

--- shapes/src/main/java/com/example/shapes/ui/ShapeFragment.kt
+++ shapes/src/main/java/com/example/shapes/ui/ShapeFragment.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import android.view.View
 import com.example.base.extensions.viewLifecycleScoped
 import com.example.base.screen.BaseFragment
-import com.example.jarassignment.R
-import com.example.jarassignment.databinding.FragmentShapeBinding
+import com.example.shapes.R
+import com.example.shapes.databinding.FragmentShapeBinding
+import com.example.shapes.view.RandomCircles
 
 
 class ShapeFragment : BaseFragment(R.layout.fragment_shape) {
 
-    private val binding: FragmentShapeBinding by viewLifecycleScoped(FragmentShapeBinding::bind)
+    private val binding: FragmentShapeBinding by viewLifecycleScoped(
+        FragmentShapeBinding::bind)
+
+    var circleList= mutableListOf<View>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        binding.btAddCircle.setOnClickListener {
-//            val circle = RandomCircles(requireContext())
-//            binding.circleContainer.addView(circle)
-//
-//        }
+       binding.apply {
+           btAddCircle.setOnClickListener {
+               val circle=RandomCircles(requireContext())
+               circleContainer.addView(circle)
+               circleList.add(circle)
+           }
+       }
+
+        binding.btUndo.setOnClickListener {
+            binding.circleContainer.removeView(circleList[circleList.size-1])
+        }
     }
 }
