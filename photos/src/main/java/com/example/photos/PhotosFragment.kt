f867e85179e60db1a993ff7f3d9a01e5d7a85f70
karthik <karthik.kolanji90@gmail.com>
2021-06-23 23:43:07 +0530

    adds hilt

--- photos/src/main/java/com/example/photos/PhotosFragment.kt
+++ photos/src/main/java/com/example/photos/PhotosFragment.kt
@@ -5,7 +5,9 @@ import android.view.View
 import com.example.base.extensions.viewLifecycleScoped
 import com.example.base.screen.BaseFragment
 import com.example.photos.databinding.FragmentPhotosBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PhotosFragment : BaseFragment(R.layout.fragment_photos) {
 
     private val binding: FragmentPhotosBinding by viewLifecycleScoped(FragmentPhotosBinding::bind)
