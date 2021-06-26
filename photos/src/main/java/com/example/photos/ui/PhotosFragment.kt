c8cc89c2e32f3d17722dec294cf195985015eb3a
karthik <karthik.kolanji90@gmail.com>
2021-06-27 03:57:28 +0530

    change to gridlayout

--- photos/src/main/java/com/example/photos/ui/PhotosFragment.kt
+++ photos/src/main/java/com/example/photos/ui/PhotosFragment.kt
@@ -1,11 +1,14 @@
 package com.example.photos.ui
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.example.base.extensions.gone
 import com.example.base.extensions.viewLifecycleScoped
 import com.example.base.extensions.visible
@@ -16,6 +19,7 @@ import com.example.photos.databinding.FragmentPhotosBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
+
 @AndroidEntryPoint
 class PhotosFragment : Fragment(R.layout.fragment_photos) {
 
@@ -24,7 +28,27 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
         initData()
+    }
+
+    private fun initView() {
+        val spacing = resources.getDimensionPixelSize(R.dimen.item_spacing)
+        binding.apply {
+            rvPhotos.setPadding(spacing, spacing, spacing, spacing)
+            rvPhotos.clipToPadding = false
+            rvPhotos.clipChildren = false
+            rvPhotos.addItemDecoration(object : ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    outRect.set(spacing, spacing, spacing, spacing);
+                }
+            })
+        }
     }
 
     private fun initData() {
@@ -68,9 +92,9 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
     private fun showData(photos: Photos) {
         viewModel.photosAdapter.setPhotos(photos)
         binding.apply {
+            rvPhotos.adapter = viewModel.photosAdapter
             viewLoading.rootLoading.gone()
             viewError.rootError.gone()
-            rvPhotos.adapter = viewModel.photosAdapter
             rvPhotos.visible()
         }
 
